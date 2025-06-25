4210569e6b4f2037f0554ee202f0154ed3433123
Marcos Tavares <72478803+MarcosHCTavares@users.noreply.github.com>
2025-06-25 18:13:55 -0300

    Update registro_tomada.dart

--- lib/models/registro_tomada.dart
+++ lib/models/registro_tomada.dart
@@ -1,1 +1,12 @@
+class RegistroTomada {
+  final DateTime dataHora;
+  final String nomeMedicamento;
+  final bool tomou; // true: tomou, false: não tomou
+
+  RegistroTomada({
+    required this.dataHora,
+    required this.nomeMedicamento,
+    required this.tomou,
+  });
+}
 
