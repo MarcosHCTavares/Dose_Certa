aed99360f0a8c80896888f42227ea9e1e8682f34
Marcos Tavares <72478803+MarcosHCTavares@users.noreply.github.com>
2025-06-25 18:09:02 -0300

    Update medicamento.dart

--- lib/models/medicamento.dart
+++ lib/models/medicamento.dart
@@ -1,1 +1,14 @@
+class Medicamento {
+  String nome;
+  String dosagem;
+  List<String> horarios; // formato "08:00", "14:00"
+  DateTime? dataFinal;
+
+  Medicamento({
+    required this.nome,
+    required this.dosagem,
+    required this.horarios,
+    this.dataFinal,
+  });
+}
 
