4aa1ccfce462534c9ec0f5844b46a5f6c4f50f2c
Marcos Tavares <72478803+MarcosHCTavares@users.noreply.github.com>
2025-06-25 18:13:43 -0300

    Update sintoma.dart

--- lib/models/sintoma.dart
+++ lib/models/sintoma.dart
@@ -1,1 +1,14 @@
+class Sintoma {
+  final DateTime data;
+  final String descricao;
+  final String intensidade; // leve, moderado, grave
+  final String? observacoes;
+
+  Sintoma({
+    required this.data,
+    required this.descricao,
+    required this.intensidade,
+    this.observacoes,
+  });
+}
 
